7eef23cb7a96a5dc404205eb122b7e4b7c1a0abc
CorentinDore <CorentinDore>
2017-10-22 18:54:23 +0200

    Add src

---
+++ src/main/java/isen/cdore/App.java
@@ -0,0 +1,13 @@
+package isen.cdore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World PA165!" );
+    }
+}
